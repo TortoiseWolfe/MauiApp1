e5d0fff5eb31f1598da6d07d0cb06d9e17cde5ae
Turtle Wolfe <JonPohlner@gmail.com>
2023-04-08 13:40:48 -0400

    Add project files.

---
+++ MauiApp1/Services/ChatbotService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Services
+{
+    internal class ChatbotService
+    {
+        public async Task<string> GetResponseAsync(string userInput)
+        {
+            await Task.Delay(500); // Simulate a small delay
+            return $"Chatbot: {userInput}";
+        }
+    }
+}
